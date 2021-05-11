3fee591b4caae99441f46f20370075f1815a8d26
anayw2001 <awadhera@berkeley.edu>
2021-05-11 21:09:20 +0530

    vendor: allow customizing the length of gesture navbar [3/3]
    Change-Id: I3ad75e27e483ce38b09db8f8f4ac467818e8ddde
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -5,6 +5,8 @@ PRODUCT_PACKAGES += \
     BuiltInPrintServiceOverlay \
     CustomPixelLauncherOverlay \
     NavigationBarModeGesturalOverlayFS \
+    GesturalNavigationOverlayLong \
+    GesturalNavigationOverlayMedium \
     GooglePermissionControllerOverlay \
     PixelConfigOverlayCommon \
     TeleServiceOverlay \

ca63f8d430d2801d40c76ed15d119f9025284a5d
johnmart19 <johnivan19999@gmail.com>
2022-03-09 19:15:57 +0200

    PixelPlusUI: V4.3 Release

--- config/branding.mk
+++ config/branding.mk
@@ -2,7 +2,7 @@
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL
 
 # PixelPlusUI Props
-PPUI_BASE_VERSION = 4.2
+PPUI_BASE_VERSION = 4.3
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
